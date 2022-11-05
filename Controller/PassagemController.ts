116d98b496216635a7a2ab1888a7dcda59cafef8
Arthur Amaral <arthur.ramos.amaral@hotmail.com>
2022-11-05 10:29:02 -0300

    criacao controller passagem

--- Controller/PassagemController.ts
+++ Controller/PassagemController.ts
@@ -1,30 +1,19 @@
-// import { RequestHandler } from "express";
-// import { IFilterPessoa, IPessoa } from "../Interfaces/IPessoa";
-// import { IPessoaService } from "../Service/IPessoaService";
+import { RequestHandler } from "express";
+import { IPassagem } from "../Interfaces/IPassagem";
+import { IPassagemService } from "../Service/IPassagemService"
 
-// export default class PassagemController {
-//   private pessoaService: IPassagemService;
-//   constructor(pessoaService: IPassagemService) {
-//     this.pessoaService = pessoaService;
-//   }
+export default class PassagemController {
+	private passagemService: IPassagemService;
+	constructor(passagemService: IPassagemService) {
+		this.passagemService = passagemService;
+	}
+	emitPassagem: RequestHandler = (request, response) => {
+		try {
+			const passagem = this.passagemService.emitPassagem(request.body as IPassagem);
+			response.status(201).send({ passagem });
+		} catch (error: any) {
+			response.status(400).send({ error: error.message })
+		}
+	}
 
-//   createPerson: RequestHandler = (request, response) => {
-//     try {
-//       const person = this.pessoaService.createPerson(request.body as IPessoa);
-//       response.status(201).send({ person });
-//     } catch (error: any) {
-//       response.status(400).send({ error: error.message });
-//     }
-//   };
-
-//   listPerson: RequestHandler = (request, response) => {
-//     try {
-//       const persons = this.pessoaService.listPerson(
-//         request.query as IFilterPessoa
-//       );
-//       response.status(200).send({ pessoas: persons });
-//     } catch (error: any) {
-//       response.status(400).send({ error: error.message });
-//     }
-//   };
-// }
+}
