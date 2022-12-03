deecc031af942228082fa99c9247bfcc92c25532
Rafael Neves <rafaelneves.130@gmail.com>
2022-12-03 19:13:18 -0300

    refactor: correct name of class

--- Factories/FlightServiceFactory.ts
+++ Factories/FlightServiceFactory.ts
@@ -2,7 +2,7 @@ import FlightService from "../Service/FlightService";
 import FlightRepository from "../Repository/FlightRepository";
 import { IFlightRepository } from "../Repository/IFlightRepository";
 
-export default class ExchangeServiceFactory {
+export default class FlightServiceFactory {
   static make(flightRepository?: IFlightRepository) {
     const flightRepositoryInstance = flightRepository || new FlightRepository();
     return new FlightService(flightRepositoryInstance);
