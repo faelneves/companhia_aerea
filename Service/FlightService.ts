1d31bcd062d13c76b42da7cf808b43216dfdb639
Rafael Neves <rafaelneves.130@gmail.com>
2022-12-03 20:26:57 -0300

    refactor: remove unreachable situation

--- Service/FlightService.ts
+++ Service/FlightService.ts
@@ -1,40 +1,37 @@
 import { IFilterFlight, IFlight, Status } from "../Interfaces/IFlight";
 import { IFlightRepository } from "../Repository/IFlightRepository";
 export default class FlightService {
-	private flightRepository: IFlightRepository;
-	constructor(flightRepository: IFlightRepository) {
-		this.flightRepository = flightRepository;
-	}
+  private flightRepository: IFlightRepository;
+  constructor(flightRepository: IFlightRepository) {
+    this.flightRepository = flightRepository;
+  }
 
-	verifyIfOriginCityIsEqualToDestinationCity(flight: IFlight) {
-		if (flight.origin.city == flight.destination.city) {
-			throw new Error("a origem não poder ser igual ao destino");
-		}
-	}
+  verifyIfOriginCityIsEqualToDestinationCity(flight: IFlight) {
+    if (flight.origin.city == flight.destination.city) {
+      throw new Error("a origem não poder ser igual ao destino");
+    }
+  }
 
-	AddZeroInFlightOcupationIfItDoesntExists(flight: IFlight) {
-		if (!flight.ocupation) {
-			flight.ocupation = 0;
-		}
-	}
+  AddZeroInFlightOcupationIfItDoesntExists(flight: IFlight) {
+    if (!flight.ocupation) {
+      flight.ocupation = 0;
+    }
+  }
 
-	createFlight(flight: IFlight) {
-		this.verifyIfOriginCityIsEqualToDestinationCity(flight)
-		this.AddZeroInFlightOcupationIfItDoesntExists(flight)
-		return this.flightRepository.createFlight(flight);
-	}
+  createFlight(flight: IFlight) {
+    this.verifyIfOriginCityIsEqualToDestinationCity(flight);
+    this.AddZeroInFlightOcupationIfItDoesntExists(flight);
+    return this.flightRepository.createFlight(flight);
+  }
 
-	listFlight(params: IFilterFlight) {
-		return this.flightRepository.filterFlight(params);
-	}
+  listFlight(params: IFilterFlight) {
+    return this.flightRepository.filterFlight(params);
+  }
 
-	getFlightById(id: string) {
-		return this.flightRepository.getFlightById(id);
-	}
-	changeStatus(id: string, status: Status) {
-		if (!status) {
-			throw new Error("status invalido");
-		}
-		return this.flightRepository.changeStatus(id, status);
-	}
+  getFlightById(id: string) {
+    return this.flightRepository.getFlightById(id);
+  }
+  changeStatus(id: string, status: Status) {
+    return this.flightRepository.changeStatus(id, status);
+  }
 }
